c22b3dc47cc19ea8003b0942c653a7bf641d983a
thankhanh <hathanhkhanh55204@gmail.com>
2026-03-10 22:36:54 +0700

    setup backend và connect db thành công - final

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING);
+        console.log("Liên kết CSDL thành công!");
     } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error.message}`);
+        console.log("Lỗi khi kết nối CSDL:", error);
         process.exit(1);
     }
 };
