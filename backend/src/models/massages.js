2d8ddef265ad7f59f56409e7bf465ce89cb1efb2
langvuong09 <cuongcaotien9a@gmail.com>
2026-03-11 17:42:44 +0700

    insert file massage.js and file conversations.js

---
+++ backend/src/models/massages.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const massageSchema = new mongoose.Schema({
+  conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true,
+    index: true,
+  },
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+    content: {
+        type: String,
+        trim: true,
+  },
+  imgUrl: {
+    type: String,
+  },
+},
+{
+  timestamps: true,
+});
+
+messageSchema.index({ conversationId: 1, createdAt: -1 });
+
+
+const Message = mongoose.model('Message', massageSchema);
+
+export default Message;
