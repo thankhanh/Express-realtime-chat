ab7f57bc793d4f9686016569cd0e96056a53188b
thankhanh <hathanhkhanh55204@gmail.com>
2026-03-17 20:58:41 +0700

    fix lỗi sever

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,52 +2,52 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./libs/db.js";
 import authRoute from "./routes/authRoute.js";
-// import userRoute from "./routes/userRoute.js";
-// import friendRoute from "./routes/friendRoute.js";
-// import messageRoute from "./routes/messageRoute.js";
-// import conversationRoute from "./routes/conversationRoute.js";
+import userRoute from "./routes/userRoute.js";
+import friendRoute from "./routes/friendRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import conversationRoute from "./routes/conversationRoute.js";
 import cookieParser from "cookie-parser";
-// import { protectedRoute } from "./middlewares/authMiddleware.js";
-// import cors from "cors";
-// import swaggerUi from "swagger-ui-express";
-// import fs from "fs";
-// import { app, server } from "./socket/index.js";
-// import { v2 as cloudinary } from "cloudinary";
+import { protectedRoute } from "./middlewares/authMiddleware.js";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import { app, server } from "./socket/index.js";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 5001;
 
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // CLOUDINARY Configuration
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // swagger
-// const swaggerDocument = JSON.parse(fs.readFileSync("./src/swagger.json", "utf8"));
+const swaggerDocument = JSON.parse(fs.readFileSync("./src/swagger.json", "utf8"));
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // public routes
 app.use("/api/auth", authRoute);
 
 // private routes
-// app.use(protectedRoute);
-// app.use("/api/users", userRoute);
-// app.use("/api/friends", friendRoute);
-// app.use("/api/messages", messageRoute);
-// app.use("/api/conversations", conversationRoute);
+app.use(protectedRoute);
+app.use("/api/users", userRoute);
+app.use("/api/friends", friendRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/conversations", conversationRoute);
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`server bắt đầu trên cổng ${PORT}`);
     });
 });
