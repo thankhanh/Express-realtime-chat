0e5bddff91bc71e083a9bdae8235b6960320460b
thankhanh <hathanhkhanh55204@gmail.com>
2026-03-19 20:51:45 +0700

    Merge branch Khanh into main

--- backend/src/models/conversation.js
+++ backend/src/models/conversation.js
@@ -1,95 +1,101 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const participantsSchema = new mongoose.Schema({
-    userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    },
-    joinedAt: {
-    type: Date,
-    default: Date.now,
-    },
-  },
-  {
-    _id: false,
-  }
-);
-
-const groupSchema = new mongoose.Schema({
-    name: {
-    type: String,
-    trim: true,
-    },
-    createBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    },
-},
-{
-    _id: false,
-});
-
-const lastMessageSchema = new mongoose.Schema({
-    _id: {type: String},
-    content: {
-    type: String,
-    default: null,
-    },
-    senderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    },
-    createdAt: {
-    type: Date,
-    default: null,
-    },
+const participantSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        joinedAt: {
+            type: Date,
+            default: Date.now,
+        },
     },
     {
-    _id: false,
+        _id: false,
     }
 );
 
-const massageSchema = new mongoose.Schema({
-    type: {
-    type: String,
-    enum: ['text', 'image'],
-    required: true,
-  },
-  participants: {
-    type: [participantsSchema],
-    required: true,
-  },
-  group:{
-    type: groupSchema,
-  },
-  lastMessageAt: {
-    type: Date,
-  },
-  seenBy: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: 'User',
-  },
-  lastMessage: {
-    type: lastMessageSchema,
-    default: null,
-  },
-  unreadCounts:{
-    type: Map,
-    of: Number,
-    default: {},
-  }
-},
-{
-  timestamps: true,
+const groupSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    {
+        _id: false,
+    }
+);
 
-});
+const lastMessageSchema = new mongoose.Schema(
+    {
+        _id: { type: String },
+        content: {
+            type: String,
+            default: null,
+        },
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        createdAt: {
+            type: Date,
+            default: null,
+        },
+    },
+    {
+        _id: false,
+    }
+);
 
-conversationSchama.index({ 
+const conversationSchema = new mongoose.Schema(
+    {
+        type: {
+            type: String,
+            enum: ["direct", "group"],
+            required: true,
+        },
+        participants: {
+            type: [participantSchema],
+            required: true,
+        },
+        group: {
+            type: groupSchema,
+        },
+        lastMessageAt: {
+            type: Date,
+        },
+        seenBy: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+        lastMessage: {
+            type: lastMessageSchema,
+            default: null,
+        },
+        unreadCounts: {
+            type: Map,
+            of: Number,
+            default: {},
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+conversationSchema.index({
     "participant.userId": 1,
     lastMessageAt: -1,
- });
+});
 
- const Concersation = mongoose.model('Conversation', conversationSchema);
-
- export default Concersation;
+const Conversation = mongoose.model("Conversation", conversationSchema);
+export default Conversation;
