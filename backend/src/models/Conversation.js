48812a7f3f81073adfeef5b887860e2d02af11ef
thankhanh <hathanhkhanh55204@gmail.com>
2026-03-30 21:56:02 +0700

    hoan thien UI auth

--- backend/src/models/Conversation.js
+++ backend/src/models/Conversation.js
@@ -1,100 +1,100 @@
 import mongoose from "mongoose";
 
 const participantSchema = new mongoose.Schema(
-    {
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        joinedAt: {
-            type: Date,
-            default: Date.now,
-        },
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {
-        _id: false,
-    }
+    joinedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    _id: false,
+  }
 );
 
 const groupSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            trim: true,
-        },
-        createdBy: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
+  {
+    name: {
+      type: String,
+      trim: true,
     },
-    {
-        _id: false,
-    }
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    _id: false,
+  }
 );
 
 const lastMessageSchema = new mongoose.Schema(
-    {
-        _id: { type: String },
-        content: {
-            type: String,
-            default: null,
-        },
-        senderId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
-        createdAt: {
-            type: Date,
-            default: null,
-        },
+  {
+    _id: { type: String },
+    content: {
+      type: String,
+      default: null,
     },
-    {
-        _id: false,
-    }
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    createdAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    _id: false,
+  }
 );
 
 const conversationSchema = new mongoose.Schema(
-    {
-        type: {
-            type: String,
-            enum: ["direct", "group"],
-            required: true,
-        },
-        participants: {
-            type: [participantSchema],
-            required: true,
-        },
-        group: {
-            type: groupSchema,
-        },
-        lastMessageAt: {
-            type: Date,
-        },
-        seenBy: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-            },
-        ],
-        lastMessage: {
-            type: lastMessageSchema,
-            default: null,
-        },
-        unreadCounts: {
-            type: Map,
-            of: Number,
-            default: {},
-        },
+  {
+    type: {
+      type: String,
+      enum: ["direct", "group"],
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    participants: {
+      type: [participantSchema],
+      required: true,
+    },
+    group: {
+      type: groupSchema,
+    },
+    lastMessageAt: {
+      type: Date,
+    },
+    seenBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lastMessage: {
+      type: lastMessageSchema,
+      default: null,
+    },
+    unreadCounts: {
+      type: Map,
+      of: Number,
+      default: {},
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 conversationSchema.index({
-    "participant.userId": 1,
-    lastMessageAt: -1,
+  "participant.userId": 1,
+  lastMessageAt: -1,
 });
 
 const Conversation = mongoose.model("Conversation", conversationSchema);
